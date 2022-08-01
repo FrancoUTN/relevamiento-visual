2d77cdffdaf4a6fad4fa5e11278e58a1c2c540e5
unknown <cataniafrancoar@gmail.com>
2022-08-01 11:37:31 -0300

    Fin login

--- src/components/ui/FormularioLogin.js
+++ src/components/ui/FormularioLogin.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { Colores } from '../../constants/estilos';
 import { useNavigation } from '@react-navigation/native';
@@ -68,59 +68,81 @@ export default function FormularioLogin({ onAuthenticate }) {
   return (
     <View style={styles.container}>
 
-        <Text style={styles.ingresarComo}>
-            Ingresar como:
-        </Text>
-        <View style={styles.boton}>
-            <Button
-                onPress={accesoAdminHandler}
-                title='Administrador'
-                color={Colores.primarioOscuro}
-            />
-        </View>
-        <View style={styles.boton}>
-            <Button
-                onPress={accesoInvitadoHandler}
-                title='Invitado'
-                color={Colores.primarioOscuro}
-            />
-        </View>
-        <View style={styles.boton}>
-            <Button
-                onPress={accesoUsuarioHandler}
-                title='Usuario'
-                color={Colores.primarioOscuro}
-            />
+        <View style={styles.botonesContainer}>
+            <Text style={styles.ingresarComo}>
+                Ingresar como...
+            </Text>
+            <View style={styles.boton}>
+                <Button
+                    onPress={accesoAdminHandler}
+                    title='Admin'
+                    color={Colores.primarioOscuro}
+                />
+            </View>
+            <View style={styles.boton}>
+                <Button
+                    onPress={accesoInvitadoHandler}
+                    title='Invitado'
+                    color={Colores.primarioOscuro}
+                />
+            </View>
+            <View style={styles.boton}>
+                <Button
+                    onPress={accesoUsuarioHandler}
+                    title='Usuario'
+                    color={Colores.primarioOscuro}
+                />
+            </View>
+            <View style={styles.boton}>
+                <Button
+                    onPress={accesoAnonimoHandler}
+                    title='Anónimo'
+                    color={Colores.primarioOscuro}
+                />
+            </View>
+            <View style={styles.boton}>
+                <Button
+                    onPress={accesoTesterHandler}
+                    title='Tester'
+                    color={Colores.primarioOscuro}
+                />
+            </View>
         </View>
 
-        <View style={styles.inputContainer}>
-            <Text style={[styles.label, credentialsInvalid.email && styles.labelInvalid]}>
-                Correo electrónico
-            </Text>
-            <TextInput
-                style={[styles.input, credentialsInvalid.email && styles.inputInvalid]}
-                keyboardType="email-address"
-                onChangeText={setCorreo}
-                value={correo}
-            />
-        </View>
-        <View style={styles.inputContainer}>
-            <Text style={[styles.label, credentialsInvalid.clave && styles.labelInvalid]}>
-                Clave
-            </Text>
-            <TextInput
-                style={[styles.input, credentialsInvalid.clave && styles.inputInvalid]}
-                secureTextEntry={true}
-                onChangeText={setClave}
-                value={clave}
-            />
-        </View>
-        <View>
-            <Button
-                onPress={submitHandler}
-                title='Acceder'
-            >
-            </Button>
+        <View style={styles.formContainer}>
+            <View style={styles.inputContainer}>
+                <Text style={[styles.label, credentialsInvalid.email && styles.labelInvalid]}>
+                    Correo electrónico
+                </Text>
+                <TextInput
+                    style={[styles.input, credentialsInvalid.email && styles.inputInvalid]}
+                    keyboardType="email-address"
+                    onChangeText={setCorreo}
+                    value={correo}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <Text style={[styles.label, credentialsInvalid.password && styles.labelInvalid]}>
+                    Contraseña
+                </Text>
+                <TextInput
+                    style={[styles.input, credentialsInvalid.password && styles.inputInvalid]}
+                    secureTextEntry={true}
+                    onChangeText={setClave}
+                    value={clave}
+                />
+            </View>
+            <View style={styles.submit}>
+                <Pressable
+                    onPress={submitHandler}
+                >
+                    <Text                        
+                        style={styles.acceder}                    
+                    >
+                        Acceder
+                    </Text>
+                </Pressable>
+            </View>
         </View>
     </View>
   );
@@ -132,17 +154,27 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    botonesContainer: {
+        flex: 1,
+        margin: 20,
+        width: '60%',
+        alignItems: 'center'
+    },
     boton: {
-        margin: 6
+        margin: 6,
+        width: '80%',
     },
     ingresarComo: {
-        fontSize: 28,
+        fontSize: 22,
         color: 'white',
         margin: 10
     },
-    inputContainer: {
-        marginVertical: 8,
+    formContainer: {
+        flex: 1,
         width: '60%'
+    },
+    inputContainer: {
+        marginVertical: 8
     },
     label: {
         color: 'white',
@@ -157,9 +189,22 @@ const styles = StyleSheet.create({
         paddingHorizontal: 6,
         backgroundColor: 'white',
         borderRadius: 4,
-        fontSize: 20,
+        fontSize: 18,
+        color: Colores.primarioOscuro,
     },
     inputInvalid: {
         backgroundColor: Colores.errorClaro,
     },
+    submit: {
+        margin: 10,
+        padding: 10,
+        borderRadius: 4,
+        backgroundColor: Colores.primarioOscuro
+    },
+    acceder: {
+        color: Colores.primarioClaro,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18
+    }
 });
