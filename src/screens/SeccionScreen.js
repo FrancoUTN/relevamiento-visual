f9eff947866c6b1a20cd946531aed5eabfbeface
unknown <cataniafrancoar@gmail.com>
2022-08-02 04:35:25 -0300

    Activo cámara

--- src/screens/SeccionScreen.js
+++ src/screens/SeccionScreen.js
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import Camara from '../components/camara/Camara';
+import IconButton from '../components/ui/IconButton';
 
 import { Colores } from '../constants/estilos';
 
 
 export default function SeccionScreen({ navigation, route }) {
     const cosas = route.params?.cosas;
+    const [tomarFoto, setTomarFoto] = useState(false);
 
     useEffect(
         () => navigation.setOptions({ title: cosas }),
@@ -19,11 +22,37 @@ export default function SeccionScreen({ navigation, route }) {
     //     console.log('Hola, ' + cosas)
     // }
 
+    const viewTemporal = (
+        <View style={{
+            margin: 20,
+            marginHorizontal: 145,
+            alignItems: 'center',
+            backgroundColor: Colores.primarioClaro,
+            borderRadius: 10
+        }}>
+          <IconButton
+            icon='camera-outline'
+            color='white'
+            size={50}
+            onPress={() => setTomarFoto(!tomarFoto) }
+          />
+        </View>
+    )
+
     return (
         <View style={styles.container}>
-            <Text>
-                {cosas}
-            </Text>
+
+            { viewTemporal }
+
+            {
+                tomarFoto ?
+                <Camara/>
+                :
+                <Text>
+                    {cosas}
+                </Text>
+            }
+            
         </View>
     );
 }
