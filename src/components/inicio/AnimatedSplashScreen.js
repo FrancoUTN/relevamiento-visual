cd5d2fc8ba9faf4788eb93375a21b7703e6834d1
unknown <cataniafrancoar@gmail.com>
2022-08-04 15:17:14 -0300

    A ver si corrigó blink animación

--- src/components/inicio/AnimatedSplashScreen.js
+++ src/components/inicio/AnimatedSplashScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Animated, Image, StyleSheet, View } from "react-native";
 import Constants from "expo-constants";
 import * as SplashScreen from "expo-splash-screen";
@@ -6,53 +6,62 @@ import * as SplashScreen from "expo-splash-screen";
 SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function AnimatedSplashScreen({ children, icono, texto }) {
-    const animation = useMemo(() => new Animated.Value(0), []);
-    const [animationComplete, setAnimationComplete] = useState(false);
+  const animation = useMemo(() => new Animated.Value(0), []);
+  const [animationComplete, setAnimationComplete] = useState(false);
+        
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+    } catch (e) {
+      console.log(e); // Útil
+    } finally {
+      efecto();
+    }
+  }, []);
+
+  const efecto = () => {
+    SplashScreen.hideAsync();
     
-    useEffect(() => {
-      // setTimeout(() => SplashScreen.hideAsync(), 300);
-      SplashScreen.hideAsync();
-      
-      Animated.timing(animation, {
-        toValue: 1,
-        duration: 5000,
-        useNativeDriver: true,
-      }).start(() => setAnimationComplete(true));      
-    }, []);
-  
-    return (
-      <View style={{ flex: 1 }}>
-        {animationComplete && children}
-        {!animationComplete && (
-          <Animated.View
-            pointerEvents="none"
-            style={[
-              StyleSheet.absoluteFill,
-              {
-                backgroundColor: Constants.manifest.splash.backgroundColor,
-              },
-            ]}
-          >
-            <Image
-              style={{
-                width: "100%",
-                height: "100%",
-                position: 'absolute'
-              }}
-              source={icono}
-            />
-            <Animated.Image
-              style={{
-                width: "100%",
-                height: "100%",
-                opacity: animation
-              }}
-              source={texto}
-              // onLoadEnd={onImageLoaded}
-              fadeDuration={0}
-            />
-          </Animated.View>
-        )}
-      </View>
-    );
-  }
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start(() => setAnimationComplete(true));  
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      {animationComplete && children}
+      {!animationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+            },
+          ]}
+        >
+          <Image
+            style={{
+              width: "100%",
+              height: "100%",
+              position: 'absolute'
+            }}
+            source={icono}
+          />
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              opacity: animation
+            }}
+            source={texto}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
