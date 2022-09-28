a3d826890ad0185be934a2edd3fa382a33b1e584
unknown <cataniafrancoar@gmail.com>
2022-09-28 02:40:22 -0300

    Charts minimalistas

--- src/screens/GraficoScreen.js
+++ src/screens/GraficoScreen.js
@@ -1,49 +1,80 @@
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import {
-    LineChart,
-    BarChart,
-    PieChart,
-    ProgressChart,
-    ContributionGraph,
-    StackedBarChart
-  } from "react-native-chart-kit";
+import { BarChart, PieChart } from "react-native-chart-kit";
+import { Colores } from '../constants/estilos';
 
 export default function GraficoScreen({ navigation, route }) {
+    const screenWidth = Dimensions.get("window").width;
+    const chartConfig = {
+        color: () => Colores.primarioClaro,
+    };
+
     const data = {
         labels: ["January", "February", "March", "April", "May", "June"],
         datasets: [
           {
-            data: [20, 45, 28, 80, 99, 43]
+            data: [1, 2, 0, 3, 5, 7]
           }
         ]
     };
 
-    const chartConfig = {
-        backgroundGradientFrom: "#1E2923",
-        backgroundGradientFromOpacity: 0,
-        backgroundGradientTo: "#08130D",
-        backgroundGradientToOpacity: 0.5,
-        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-        strokeWidth: 2, // optional, default 3
-        barPercentage: 0.5,
-        useShadowColorFromDataset: false // optional
-    };
+    const legendFontSize = 14;
+    const data2 = [
+        {
+          name: "Seoul",
+          population: 0,
+          color: "rgba(131, 167, 234, 1)",
+          legendFontColor: "#222",
+          legendFontSize
+        },
+        {
+          name: "Toronto",
+          population: 4,
+          color: "#F00",
+          legendFontColor: "#222",
+          legendFontSize
+        },
+        {
+          name: "New York",
+          population: 2,
+          color: "#ffffff",
+          legendFontColor: "#222",
+          legendFontSize
+        },
+        {
+          name: "Moscow",
+          population: 1,
+          color: "rgb(0, 0, 255)",
+          legendFontColor: "#222",
+          legendFontSize
+        }
+    ];
 
     return (
-        <BarChart
-            style={{
-                marginVertical: 8,
-                borderRadius: 16
-            }}
-            data={data}
-            width={Dimensions.get("window").width}
-            height={220}
-            yAxisLabel="$"
-            chartConfig={chartConfig}
-            verticalLabelRotation={30}
-        />
+        <View style={styles.viewPrincipal}>
+            <BarChart
+                data={data}
+                width={screenWidth}
+                height={220}
+                chartConfig={chartConfig}
+            />
+            <PieChart
+                data={data2}
+                width={screenWidth}
+                height={200}
+                chartConfig={chartConfig}
+                accessor={"population"}
+                // backgroundColor={"transparent"}
+                // paddingLeft={"50"}
+                // center={[50, 50]}
+                absolute
+                // hasLegend={false}
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    viewPrincipal: {
+        flex: 1,
+    }
 });
